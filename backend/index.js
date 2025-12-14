82016d1d468ae2acba41ad3f2120fd75bf666c18
Chandrashekhar Singh <shekhar534042@gmail.com>
2025-12-14 23:35:12 +0530

    Add root route for backend health check

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,10 @@ let port = process.env.PORT || 4000
 let app = express()
 
 app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 app.use(cookieParser())
 app.use(cors({
  origin:["http://localhost:5173" , "http://localhost:5174"],
