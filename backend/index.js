6abfcc44367ffaf2c5267d09344d41af78e3a44a
thakur-chandrashekhar <shekhar534042@gmail.com>
2026-01-28 23:12:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ app.use(cors({
  credentials:true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }))
+app.options("*", cors());
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
