864f847745f6731d7babbb8999448552f4c9815a
thakur-chandrashekhar <shekhar534042@gmail.com>
2026-04-16 00:01:13 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { createContext } from 'react'
-export const authDataContext= createContext()
-function AuthContext({children}) {
+import axios from "axios";   // ✅ add this
+
+export const authDataContext = createContext()
+
+function AuthContext({ children }) {
+
     let serverUrl = import.meta.env.VITE_BACKEND_URL;
 
-    let value = {
-       serverUrl
-    }
-  return (
+    // 🔥 IMPORTANT FIX
+    axios.defaults.withCredentials = true;
 
-    
-    <div>
+    let value = {
+        serverUrl
+    }
+
+    return (
         <authDataContext.Provider value={value}>
             {children}
         </authDataContext.Provider>
-      
-    </div>
-  )
+    )
 }
 
 export default AuthContext
